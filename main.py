594aa67cc799adc57732ef63a6b04cb7bebc40ed
Varun kasana <vkasana_be24@thapar.edu>
2025-03-29 11:12:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pywhatkit as kit
+
+data = pd.read_csv('number.csv',dtype = 'string')
+for i, row in data.iterrows():
+   print(f"Sendingmessage to {row['Name']} at {row['Number']}")
+   message = open("message.txt").read()
+   message = message.replace("{name}", row['Name'])
+   
+   
+   
+   kit.sendwhatmsg_instantly(phone_no = str(row["Number"]), message = message, tab_close = True, wait_time = 2, close_time = 2, browser = "chrome")
+   
+   
+   
+   
